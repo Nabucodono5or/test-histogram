70ef7e7ab54e6c581aabea68206f2627a264793f
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-09-04 10:33:40 -0300

    criado os axis e scalas, resolvido a formatação dos dados com histogram

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { select, selectAll } from "d3-selection";
-import { histogram } from "d3-array";
-import { csv } from 'd3-fetch';
+import { histogram, extent } from "d3-array";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
+import { axisBottom, axisLeft } from "d3-axis";
+import { max } from "d3";
 
 let width = 500;
 let height = 500;
-let margin = { top: 10, left: 10, right: 10, bottom: 10 };
+let margin = { top: 10, left: 20, right: 10, bottom: 10 };
 
-
+csv(require("./data/Video_Games_Sales_as_at_22_Dec_2016.csv")).then((data) => {
+  data.map((d) => (d.Year_of_Release = +d.Year_of_Release));
+  console.log(data);
+  render(data);
+});
 
 function render(incomingData) {
   let innerWidth = width - margin.left - margin.right;
@@ -20,6 +27,38 @@ function render(incomingData) {
     .append("g")
     .attr("transform", `translate(${margin.top}, ${margin.bottom})`);
 
-  let h = histogram();
-  console.log(h);
+  let x = scaleLinear()
+    .domain(extent(incomingData, (d) => d.Year_of_Release))
+    .range([margin.left, innerWidth]);
+
+  let xAxis = axisBottom(x);
+
+  let gXAxis = g
+    .append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${0}, ${innerHeight - margin.bottom})`)
+    .call(xAxis);
+
+  let h = histogram()
+    .domain(x.domain())
+    .value((d) => {
+      return d.Year_of_Release;
+    });
+
+  let bins = h(incomingData);
+  console.log(bins);
+
+  let y = scaleLinear()
+    .domain([0, max(bins, (d) => d.length)])
+    .range([innerHeight, 0]);
+
+  let yAxis = axisLeft(y);
+
+  let gYAxis = g
+    .append("g")
+    .attr("class", "YAxis")
+    .attr("transform", `translate(${margin.left}, ${-margin.bottom})`)
+    .call(yAxis);
+
+  
 }
